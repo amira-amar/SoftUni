e70563c7c0ad7bb633129d90188a282060976941
amira_amar <amira.amar.office@gmail.com>
2025-06-23 14:01:03 +0300

    Delete Configuration.cs

--- Configuration.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Boardgames.Data
-{
-    public static class Configuration
-    {
-        public static string ConnectionString =
-            @"Server=localhost,1433;Database=Boardgames;User Id=sa;Password=Traktor123!;TrustServerCertificate=True;Encrypt=False";
-            
-    }
-}
